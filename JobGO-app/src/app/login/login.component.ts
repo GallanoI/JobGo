88518728b3b13575d1da03987b22f939e624a056
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2024-05-08 13:27:11 -0400

    login previo

--- JobGO-app/src/app/login/login.component.ts
+++ JobGO-app/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-
+import { Router } from '@angular/router'; // Importar Router
 
 
 @Component({
@@ -8,27 +8,39 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass'
+  styleUrl: './login.component.scss'
 })
 
 export class LoginComponent implements OnInit {
   loginForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router) { // Inyectar Router
+
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
-  onSubmit() {
-    if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
+  onSubmit(): void {
+    console.log(this.loginForm.value.username)
+    console.log(this.loginForm.value.password)
+    if (this.loginForm.value.username === '123' && this.loginForm.value.password === '123') {
+      // Redirige al usuario a la pantalla de selección de rol
+      this.router.navigate(['/select-role']);
+    } else {
+      // Opcional: Mostrar un mensaje de error si los datos son incorrectos
+      alert('Usuario o contraseña incorrectos');
     }
   }
 }
