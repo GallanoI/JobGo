d6de613d1c539f8b042d6f6eef70b826e63f99ad
G <107266302+GallanoI@users.noreply.github.com>
2024-06-17 01:55:31 +0200

    Ventana de Selección de Ruta

--- JobGO-app/src/app/app.routes.ts
+++ JobGO-app/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RoleSelectionComponent } from './role-selection/role-selection.component';
 import { RegComponent } from './reg/reg.component';
+import { DriverTripComponent } from './trip/drivertrip.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'reg', component: RegComponent },
-  { path: 'select-role', component: RoleSelectionComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'role-selection', component: RoleSelectionComponent },
+  { path: 'drivertrip', component: DriverTripComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
