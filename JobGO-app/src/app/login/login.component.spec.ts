792fb585908022ffd104e2ca3160bb861d3f2881
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2024-06-17 01:12:07 -0400

    solo 20 y 22

--- JobGO-app/src/app/login/login.component.spec.ts
+++ JobGO-app/src/app/login/login.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let debug: DebugElement;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent]
