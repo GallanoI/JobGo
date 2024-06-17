7d100bfeb959e31d05155f7fd647874ac94bbca5
Miguel Espinoza <miguel.espinoza@alumnos.uv.cl>
2024-06-16 23:30:22 -0400

    Revert "Cambios en Selección de rol"
    This reverts commit 03bc8eb55a0e2e868a3581ef49bd6d0a027219ce.

--- JobGO-app/src/app/role-selection/role-selection.component.ts
+++ JobGO-app/src/app/role-selection/role-selection.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
-import { Router } from '@angular/router'; // Importar Router
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-role-selection',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './role-selection.component.html',
   styleUrl: './role-selection.component.sass'
 })
 
-export class RoleSelectionComponent implements OnInit {
-  roleSelecForm: FormGroup | undefined;
-
-  ngOnInit(): void {
-    this.roleSelecForm = new FormGroup({
-      // Define tus controles de formulario aquí
-      roleName: new FormControl('')
-    });
-  }
-
-  onSubmit(): void {
-    // Lógica para manejar el envío del formulario
+export class RoleSelectionComponent {
+  selectRole(role: string) {
+    console.log('Role selected:', role);
+    // Aquí podrías redirigir al usuario o guardar su rol
   }
 }
 
