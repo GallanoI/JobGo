9fbb0071005867cde1ed6a75d7ef3bab47ef6451
G <107266302+GallanoI@users.noreply.github.com>
2024-07-22 14:28:41 +0200

    Re-vivir la "pag"

--- JobGO-app/src/app/app.routes.ts
+++ JobGO-app/src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { DriverTripComponent } from './trip/drivertrip.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'reg', component: RegComponent },
-  { path: 'selection-role', component: RoleSelectionComponent },
+  { path: 'role-selection', component: RoleSelectionComponent },
   { path: 'drivertrip', component: DriverTripComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
